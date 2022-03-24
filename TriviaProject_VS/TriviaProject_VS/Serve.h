a83ea5e0e764d5bc753e52895cb4ba766831c506
dolevbrami <dolevbrami2005@gmail.com>
2022-03-24 17:10:46 +0200

    starting building the communicate interface

---
+++ TriviaProject_VS/TriviaProject_VS/Serve.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <WinSock2.h>
+#include <exception>
+#include <string>
+#include <thread>
+#include <mutex>
+#define TRACE(msg, ...) printf(msg "\n", __VA_ARGS__);
+class serveTool
+{
+public:
+
+private:
+
+};
--- TriviaProject_VS/TriviaProject_VS/TriviaProject_VS.cpp
+++ TriviaProject_VS/TriviaProject_VS/TriviaProject_VS.cpp
@@ -1,8 +1,19 @@
-
+#pragma comment (lib, "ws2_32.lib")
 #include <iostream>
+#include <fstream>
+#include "WSAInitializer.h"
+#include "serve.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    try {
+        TRACE("Starting...");
+        
+
+    }
+    catch (const std::exception& e) {
+        std::cout << "somthing went wrong with the server" << std::endl;
+    }
+    
 }
 
