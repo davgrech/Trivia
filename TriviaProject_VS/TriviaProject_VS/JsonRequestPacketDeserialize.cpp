3533d94d5da631a691ba61dae06386a6f10c88d5
David Grechkin <123dg32@gmail.com>
2022-04-10 19:47:08 +0300

    added login and signup deserializer

--- TriviaProject_VS/TriviaProject_VS/JsonRequestPacketDeserialize.cpp
+++ TriviaProject_VS/TriviaProject_VS/JsonRequestPacketDeserialize.cpp
@@ -1,11 +1,26 @@
 #include "JsonRequestPacketDeserialize.h"
+
+#include <nlohmann/json.hpp>
+using nlohmann::json;
+
 LoginRequest JRPD::deserializeLoginRequest(std::vector<unsigned char> buffer)
 {
+    json j;
+    LoginRequest value(
+            j["password"].get<std::string>(),
+            j["username"].get<std::string>()
+        );
+    return value;
     
-    return LoginRequest();
 }
 
 SignupRequest JRPD::deserializeSignupRequest(std::vector<unsigned char> buffer)
 {
-    return SignupRequest();
+        json j;
+    SignupRequest value(
+        j["password"].get<std::string>(),
+        j["username"].get<std::string>()
+        j["email"].get<std::string>()
+    );
+    return value;
 }
