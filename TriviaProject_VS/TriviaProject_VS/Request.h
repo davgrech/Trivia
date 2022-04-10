83ddf9005ee40e6b50a20f42b7c5925a22c200e8
dolevbrami <dolevbrami2005@gmail.com>
2022-04-10 10:16:21 +0300

    added request header file interface

---
+++ TriviaProject_VS/TriviaProject_VS/Request.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+
+struct LoginRequest {
+	std::string username;
+	std::string password;
+} typedef LoginRequest;
+
+struct SignupRequest {
+	std::string username;
+	std::string password;
+	std::string email;
+}typedef SignupRequest;
+class JRPS {
+public:
+	static LoginRequest deserializeLoginRequest(std::vector<char> buffer);
+	static SignupRequest deserializeSignupRequest(std::vector<char> buffer);
+	
