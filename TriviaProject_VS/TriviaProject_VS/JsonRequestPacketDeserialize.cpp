2ee2374eaacf32c521d70c0ae9f4d4a1e936ae4e
David Grechkin <123dg32@gmail.com>
2022-04-11 13:49:55 +0300

    fixed login and signup request des

--- TriviaProject_VS/TriviaProject_VS/JsonRequestPacketDeserialize.cpp
+++ TriviaProject_VS/TriviaProject_VS/JsonRequestPacketDeserialize.cpp
@@ -6,10 +6,9 @@ using nlohmann::json;
 LoginRequest JRPD::deserializeLoginRequest(std::vector<unsigned char> buffer)
 {
     json j;
-    LoginRequest value(
-            j["password"].get<std::string>(),
-            j["username"].get<std::string>()
-        );
+    LoginRequest value;
+    value.username = j["username"].get<std::string>();
+    value.password = j["password"].get<std::string>();
     return value;
     
 }
@@ -17,10 +16,11 @@ LoginRequest JRPD::deserializeLoginRequest(std::vector<unsigned char> buffer)
 SignupRequest JRPD::deserializeSignupRequest(std::vector<unsigned char> buffer)
 {
         json j;
-    SignupRequest value(
-        j["password"].get<std::string>(),
-        j["username"].get<std::string>()
-        j["email"].get<std::string>()
-    );
+
+        SignupRequest value;
+        
+        value.username = j["useranme"].get<std::string>();
+        value.password = j["password"].get<std::string>();
+        value.email = j["email"].get<std::string>();
     return value;
 }
