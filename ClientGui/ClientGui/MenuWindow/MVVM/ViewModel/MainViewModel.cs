682f4644e8afa111965555b4aa4a06d066e1b01f
David Grechkin <123dg32@gmail.com>
2022-05-25 02:23:46 +0300

    added highscores and user statstics relay command

--- ClientGui/ClientGui/MenuWindow/MVVM/ViewModel/MainViewModel.cs
+++ ClientGui/ClientGui/MenuWindow/MVVM/ViewModel/MainViewModel.cs
@@ -9,19 +9,48 @@ namespace ClientGui.MenuWindow.MVVM.ViewModel
 {
     class MainViewModel : ObservableObject
     {
+        public RelayCommand HomeViewCommand { get; set; } // move to home page
+        public RelayCommand UserStatisticsViewCommand { get; set; } //move to user statistics page
+        public RelayCommand HighScoresViewCommand { get; set; } //move to highscores page
+
+
+
+
+        public HomeViewModel HomeVm { get; set; } // homepage model
+
+        public UserStatisticsViewModel userStatsVM { get; set; } // userstats model
+        public HighScoresViewModel HighScoresVM { get; set; } // high scores view model
         private object _currentView;
 
         public object CurrentView
         {
             get { return _currentView; }
-            set { _currentView = value; 
-            }
+            set { _currentView = value;
+                OnPropertyChanged();
+                    }
         }
 
 
         public MainViewModel()
         {
+            HomeVm = new HomeViewModel();
+            userStatsVM = new UserStatisticsViewModel();
+            CurrentView = HomeVm;
 
+            HomeViewCommand = new RelayCommand(o => 
+            {
+                CurrentView = HomeVm;
+            });
+
+            UserStatisticsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = userStatsVM;
+            });
+
+            HighScoresViewCommand = new RelayCommand(o =>
+            {
+                HighScoresVM = new HighScoresViewModel();
+            });
         }
     }
 }
