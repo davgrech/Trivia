07054dc050abed56d3abb4e0b1fe89b87a8e80e2
David Grechkin <123dg32@gmail.com>
2022-05-28 00:12:15 +0300

    added a personal stats page xaxa

--- ClientGui/ClientGui/MenuPages/PersonalStats.xaml.cs
+++ ClientGui/ClientGui/MenuPages/PersonalStats.xaml.cs
@@ -23,5 +23,10 @@ namespace ClientGui.MenuPages
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
