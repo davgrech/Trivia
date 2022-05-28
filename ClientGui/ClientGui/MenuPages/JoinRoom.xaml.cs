0de5ded0c026751601511c1d825e2d32c3d64958
dolevbrami <dolevbrami2005@gmail.com>
2022-05-28 17:28:10 +0300

    toDavid

--- ClientGui/ClientGui/MenuPages/JoinRoom.xaml.cs
+++ ClientGui/ClientGui/MenuPages/JoinRoom.xaml.cs
@@ -25,6 +25,12 @@ namespace ClientGui.MenuPages
     {
         Socket mysock;
         string name;
+
+
+        public class joinRoomRequest
+        {
+            int roomId;
+        }
         public JoinRoom(Socket client, string userName)
         {
             InitializeComponent();
@@ -32,9 +38,24 @@ namespace ClientGui.MenuPages
             mysock = client;
             name = userName;
 
-        }
 
+            string[] x = { "hi", "my", "name", "is", "david" };
+            string getStringToParse = getRooms();
+            
+            
+            
+            myListBox.ItemsSource = x;
+
+        }
        
+       private string getRooms()
+        {
+            string msg = "70000";
+            SendInfrmaionToServer(msg);
+            string response = ReciveInformationFromServer();
+            dynamic magic = JsonConvert.DeserializeObject(response);
+            return magic["Rooms"];
+        }
         private void button_toggle(object sender, RoutedEventArgs e)
         {
             MenuHandler returnToMenu = new MenuHandler(mysock, name);
@@ -52,5 +73,55 @@ namespace ClientGui.MenuPages
             base.OnMouseLeftButtonDown(e);
             DragMove();
         }
+
+        private void myListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            txtSelectedRoom.Text = myListBox.SelectedItem.ToString();
+            txtSelectedRoom.Visibility = Visibility.Visible;
+        }
+
+
+
+
+        private void SendInfrmaionToServer(string userInfo)
+        {
+            if (mysock.Connected)
+            {
+                // 1 convert the form informatino to byte array
+                byte[] userData = Encoding.ASCII.GetBytes(userInfo);
+                // send data to the server as byte array
+                mysock.Send(userData);
+            }
+        }
+        private string ReciveInformationFromServer()
+        {
+            try
+            {
+
+                //preper to recive data from the server
+                //1.preper byte array to get all the bytes from the servr
+                byte[] reciveBuffer = new byte[2048];
+                //2.recive the data from the server in to the byte array and
+                //return the size of bvtes how recive
+                int rec = mysock.Receive(reciveBuffer);
+                //3. preper byte array with the size of bytes how recive frm
+                //the servr
+                byte[] data = new byte[rec];
+                //4. copy the byte array how reive in to the byte array with
+                //the correct size
+                Array.Copy(reciveBuffer, data, rec);
+                //5. convert the byte array to Ascii
+                string returnFormServer = Encoding.ASCII.GetString(data);
+                return returnFormServer;
+            }
+            catch (Exception e)
+            {
+                //LBserverStatus.Text = "not ok";
+            }
+            return "";
+        }
+
+
+
     }
 }
